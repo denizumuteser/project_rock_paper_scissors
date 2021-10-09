02887bea7c574248b480b1ece3096c528a9425a3
denizumuteser <denizumuteser@yahoo.com.tr>
2021-10-09 13:36:23 +0300

    all done

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,79 +1,128 @@
-let scores = [0, 0]
+let lives = [5, 5];
+let round = 1;
 
 function computerPlay()
 {
-    let hand = ["rock", "paper", "scissors"]
-    let index = Math.floor(Math.random()*3)
-    return hand[index]
+    let hand = ["rock", "paper", "scissors"];
+    let index = Math.floor(Math.random()*3);
+    return hand[index];
+}
+
+function updateHealth()
+{
+
+    const player = document.querySelectorAll('.health');
+    player[0].textContent = lives[0];
+    player[1].textContent = lives[1];
+}   
+
+function updateDisplay(text)
+{
+    const disptext = document.querySelector('#display-text');
+    disptext.textContent = text;
+}
+
+function resetAll()
+{
+    lives = [5, 5];
+    updateHealth();
+    round = 1;
+    
+    const winlose = document.querySelector('#win-lose');
+        winlose.textContent = "Round "+String(round);
+
+    updateDisplay("Start by choosing your weapon:");
+    
+}
+
+function updateAll(text)
+{
+    updateHealth();
+    updateDisplay(text);
+    round += 1;
+    const winlose = document.querySelector('#win-lose');
+        winlose.textContent = "Round "+String(round);
+    //won or lose?
+    if (lives[0] <= 0)
+    {
+        console.log("Lose")
+        const winlose = document.querySelector('#win-lose');
+        winlose.textContent = "LOSE!";
+        
+        setTimeout( () => {
+            alert("YOU LOSE!");
+            resetAll()
+        }, 100);
+
+        
+    }
+    else if (lives[1] <= 0)
+    {
+        console.log("Win")
+        const winlose = document.querySelector('#win-lose');
+        winlose.textContent = "WIN!";
+        setTimeout( () => {
+            alert("YOU WON!");
+            resetAll()
+        }, 100);
+    }
 }
 
 function playRound(playerSelection, computerSelection)
 {
     if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection == "rock") {
-            return "Tie!"
+            return "Tie! You both choose Rock";
         }
         else if (computerSelection == "paper") {
-            scores[1] += 1
-            return "You Lose! Paper beats Rock"
+            lives[0] -= 1;
+            return "You Lose! Paper beats Rock";
         }
         else if (computerSelection == "scissors") {
-            scores[0] += 1
-            return "You Win! Rock beats Scissors"
+            lives[1] -= 1;
+            return "You Win! Rock beats Scissors";
         }   
     }
     else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "paper") {
-            return "Tie!"
+            return "Tie! You both choose Paper";
         }
         else if (computerSelection == "scissors") {
-            scores[1] += 1
-            return "You Lose! Scissors beats Paper"
+            lives[0] -= 1;
+            return "You Lose! Scissors beats Paper";
         }
         else if (computerSelection == "rock") {
-            scores[0] += 1
-            return "You Win! Paper beats Rock"
+            lives[1] -= 1;
+            return "You Win! Paper beats Rock";
         }   
     }
     else if (playerSelection.toLowerCase() == "scissors") {
         if (computerSelection == "scissors") {
-            return "Tie!"
+            return "Tie! You both choose Scissors";
         }
         else if (computerSelection == "rock") {
-            scores[1] += 1
-            return "You Lose! Rock beats Scissors"
+            lives[0] -= 1;
+            return "You Lose! Rock beats Scissors";
         }
         else if (computerSelection == "paper") {
-            scores[0] += 1
-            return "You Win! Scissors beats Paper"
+            lives[1] -= 1;
+            return "You Win! Scissors beats Paper";
         }   
     }
 }
 
-
-
-function game()
-{
-    scores = [0, 0]
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock Paper or Scissors?")
-        let computerSelection = computerPlay(); 
-        console.log(playRound(playerSelection, computerSelection))        
-    }
-    console.log(scores)
-    if (scores[0] > scores[1])
-    {
-        console.log("Player Wins The Game!")
-    }
-    else if (scores[0] < scores[1])
-    {
-        console.log("Computer Wins The Game!")
-    }
-    else if (scores[0] == scores[1])
-    {
-        console.log("Tie???")
-    }
-
-}
-
-game()
+//rock
+const btn_rock = document.querySelector('#rock');
+btn_rock.addEventListener('click', () => {
+    updateAll(playRound("rock",computerPlay()))
+});
+//paper
+const btn_paper = document.querySelector('#paper');
+btn_paper.addEventListener('click', () => {
+    updateAll(playRound("paper",computerPlay()))
+});
+//scissors
+const btn_scissors = document.querySelector('#scissors');
+btn_scissors.addEventListener('click', () => {
+    updateAll(playRound("scissors",computerPlay()))
+});
